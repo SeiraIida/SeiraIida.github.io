8f8422aed684086c2bd117bdd3c7e8c9b28362a7
SeiraIida <58280197+SeiraIida@users.noreply.github.com>
2019-12-16 16:09:00 +0900

    Add design file

---
+++ _1.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:43d4a81929ce027ef8474bdd1679e43e3c7ecfa8948c7737f6e540620aa8711d
+size 33201945
---
+++ _2.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:49583a34babb1df0fe1e0b00b8a8839ae3bdca51336c6b4945d54c9f95543071
+size 100422873
